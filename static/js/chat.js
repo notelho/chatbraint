cd7e16bef56bf8ccf3cdef554d73aed99602930e
enbot <nimbotelho@gmail.com>
2020-02-25 19:25:05 -0300

    update tests page template

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,47 +1,44 @@
+var synth = window.speechSynthesis
+var msg = new SpeechSynthesisUtterance()
+var voices = synth.getVoices()
 
-$(function () {
+msg.voice = voices[0]
+msg.rate = 1
+msg.pitch = 1
 
-    var synth = window.speechSynthesis;
-    var msg = new SpeechSynthesisUtterance();
-    var voices = synth.getVoices();
+$('#chatbot-form-btn').click(function (e) {
+    e.preventDefault()
+    $('#chatbot-form').submit()
+})
 
-    msg.voice = voices[0];
-    msg.rate = 1;
-    msg.pitch = 1;
+$('#chatbot-form-btn-clear').click(function (e) {
+    e.preventDefault()
+    $('#chatPanel').find('.media-list').html('')
+})
 
-    $('#chatbot-form-btn').click(function (e) {
-        e.preventDefault();
-        $('#chatbot-form').submit();
-    });
+$('#chatbot-form').submit(function (e) {
+    e.preventDefault()
 
-    $('#chatbot-form-btn-clear').click(function (e) {
-        e.preventDefault();
-        $('#chatPanel').find('.media-list').html('');
-    });
+    $(".media-list")
+        .append(`<li class="media-user">${$('#msg').val()}<hr/></li>`)
 
-    $('#chatbot-form').submit(function (e) {
-        e.preventDefault();
-        var message = $('#msg').val();
-        $(".media-list").append('<li class="media"><div class="media-body"><div class="media"><div style = "text-align:right; color : #31b0d5" class="media-body">' + message + '<hr/></div></div></div></li>');
+    $.ajax({
+        type: "POST",
+        url: "/",
+        data: $(this).serialize(),
+        success: function (response) {
+            $('#msg').val('')
 
-        $.ajax({
-            type: "POST",
-            url: "/",
-            data: $(this).serialize(),
-            success: function (response) {
-                $('#msg').val('');
-                var answer = response.answer;
+            $(".media-list")
+                .append(`<li class="media-bot">${response.answer}<hr/></li>`);
 
-                $(".media-list").append('<li class="media"><div class="media-body"><div class="media"><div style = "color : white" class="media-body">' + answer + '<hr/></div></div></div></li>');
-                $(".fixed-panel").stop().animate({ scrollTop: $(".fixed-panel")[0].scrollHeight }, 1000);
+            $(".fixed-panel")
+                .stop()
+                .animate({ scrollTop: $(".fixed-panel")[0].scrollHeight }, 1000)
 
-                msg.text = answer;
-                speechSynthesis.speak(msg);
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        });
-    });
+            msg.text = answer
+            speechSynthesis.speak(msg)
 
-});
+        }, error: error => console.log(error)
+    })
+}) 
